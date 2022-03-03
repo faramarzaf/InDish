7ef1f194ff6287d7f6a5a9835509d935e3a25888
faramarzaf <faramarzafzali@gmail.com>
2022-03-03 21:05:36 +0330

    first commit

---
+++ app/src/main/java/com/faraf/InDishApplication.java
@@ -0,0 +1,47 @@
+package com.faraf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+public class InDishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InDishApplication.class, args);
+	}
+
+
+/*	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}*/
+
+	/**
+	 * An application context delegates the message resolution to a bean with the exact name messageSource.
+	 * ReloadableResourceBundleMessageSource is the most common MessageSource implementation that
+	 * resolves messages from resource bundles for different locales
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	/**
+	 * To use custom name messages in a properties file like we need to define a
+	 * LocalValidatorFactoryBean and register the messageSource
+	 */
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+
+}
---
+++ app/src/test/java/com/faraf/InDishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faraf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InDishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
